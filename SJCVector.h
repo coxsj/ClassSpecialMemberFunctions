246702d0b4b15b6971d086142ef38915fbcfba15
Steven Cox <coxstevenj@gmail.com>
2022-08-15 16:58:52 -0400

    Add notes. Simpify SJCVector constructors.

--- SJCVector.h
+++ SJCVector.h
@@ -10,12 +10,14 @@
 // Back To Basics: The Special Member Functions - Klaus Iglberger - CppCon 2021
 //		https://www.youtube.com/watch?v=9BM5LAvNtus
 
-#define BY_VAL_OPERATOR
 
-// This is a resource management class. Rule of three applies (Destructor, Copy Constructor, Copy Assignment).
+// Summary
+// Rule of zero. If your class manage no resources requiring manual cleanup, write no special member functions.
+// Instead, default them all.
+// If the class manages resources. Rule of three applies (Destructor, Copy Constructor, Copy Assignment).
 // Resource is anything that requires special manual management
 // e.g Allocated memory, file handles, mutex locks, c++ threads etc.
-// Whenever the rule of three applies, use the copy-and-swap idiom to implement assignement
+// Whenever the rule of three applies, use the copy-and-swap idiom to implement assignment
 // Move semantics gives us rule of five
 // Can move to rule of 4 1/2 if use by-value assignment operator
 
@@ -41,6 +43,41 @@
 // For each local scope between the throw and the catch, the runtime invokes the destructors of all local variables
 // in that scope. So, to avoid leaks, put all CLEANUP code in your DESTRUCTORS.
 // To make an object NON-COPYABLE, explicitly set the copy constructor and copy assignment operator to = delete.
+// DEFAULT special member functions. Explicitly setting special member functions to =default make code
+// selff documenting. i.e. I considered if the default special member functions were acceptable and they are
+// so I am ok with the compiler default implementation.
+// RVALUE REFERENCES are useful in generating special member functions.
+// Generally, lvalues dont bind to rvalues and rvalues dont bind to lvalues.
+// Special case of const lvalue reference will bind to rvalue!!
+// void f(int&);				f(i);	// Ok			f(42);	// Error
+// foid g(int&&);				g(i);	// Error		g(42)	// Ok
+// void h(const int&);		h(i)	// Ok			h(42)	// Ok
+// Combine this with overload resolution (and knowing that rvalues wont be missed)
+// No one cares what we do with the contents of an rvalue when passed to us.
+// MOVE CONSTRUCTOR is an rvalue parameter overload of the COPY CONSTRUCTOR
+// MOVE CONSTRUCTOR doesnt need to bother with the slow copying operations
+// just steal the guts of the rvalue. In practice, use std::exchange e.g. ptr_ = std::exchange(rhs.ptr_, nullptr);
+// (rhs.ptr_ goes to ptr_ and nullptr goes to rhs.ptr_).
+// RULEOF FIVE. The need for performance adds the MOVE CONSTRUCTOR and MOVE ASSIGNMENT OPERATOR.
+// MOVE CONSTRUCTOR - transfer ownership of resource.
+// MOVE ASSIGNMENT OPERATOR - free the left hand resource and transfer ownership of the right hand one.
+// Note: The copy constructor and the move assignment operator are very similar, difffering only in the std::move in
+// the move assignment operator. And this is how the STL does these special member functions.
+// However this provides an opportunity to set both assignment operators up as call by value, leaving the copy 
+// decision up to the caller.
+// This leads to the RULE OF FOUR AND A HALF - destructor, copy constructor, move constructor, by-value
+// assignment operator, and (1/2) a non-member swap function, and ideally a member version too. (If you don't,
+// std::swap gets called which in turn calls move assignment which calls swap...)
+// Note the by-value assignment operator requires passing a copy of the object on the stack to the operator.
+// This will be slow for large objects.
+// UNIQUE_PTR helps us move closer to the rule of zero.
+// By managing ownership of member resources with unique_ptr, we can delegate handling transfer to unique_ptr.
+// This works because std::unique_ptr CAN ONLY BE MOVED, its copy constructor is deleted.
+// MOVE CONSTRUCTOR with unique_ptr becomes Classname(Classname&& rhs noexcept = default;
+// DESTRUCTOR with unique_ptr becomes ~Classname() = default; // std::unique_ptr destroys the resource.
+// wherein the transfer of owenership is handled by unique_ptr when it is moved into the newly constructed object.
+
+#define BY_VAL_OPERATOR
 
 class SJCVector {
 	std::unique_ptr<int[]> ptr_ = nullptr;		//Class manages resource
@@ -50,16 +87,15 @@ class SJCVector {
 	std::string name_{ "unnamed" };
 
 public:
-	SJCVector() { 
-		initSJCVector(1);  
+	SJCVector() : SJCVector(1) { 
 		std::cout << "Standard ctor\n"; 
 	}
 	SJCVector(std::size_t size) { 
 		initSJCVector(size); 
 		std::cout << "Standard ctor with size\n"; 
 	}
-	SJCVector(std::string name, size_t size=1) : name_(name) {
-		initSJCVector(size); 
+	SJCVector(std::string name, size_t size=1) : SJCVector(size) {
+		name_ = name;
 		std::cout << "Standard ctor with name " << name_ << std::endl;
 	}
 	// Destructor needed to free the resource. (But not if you use smart pointers to ref the resource).
@@ -76,24 +112,24 @@ public:
 	// Compiler generated default copy constructor just does an element by element copy,
 	// even for heap pointers, file handles etc.
 	// This leads to DOUBLE-FREEING if there is a Destructor (which frees the resource) and only 
-	// the default copy constructor (which copies just the pointer/Handle etc, but does not create a new resource
-	// thus leaving them pointing to the same resource owned by the object that was copied.
+	// the default copy constructor (which copies just the pointer/Handle etc, but does not create a new resource).
+	// The default copy constructor leaves ptrs pointing to the same resource owned by the object that was copied.
 	// And of course we cant just give the new object the resource belonging to the rhs object!!
 	SJCVector(const SJCVector& rhs) {
 		std::cout << "Copy ctor. Copying data from ";
 		rhs.printName(); std::cout << "to "; printNameLn();
-		//Make sure to delete any existing resource before creating a new one
-		//ptr_ = new int[rhs.last_ + 1];
+		// Make sure to delete any existing resource before creating a new one
+		// ptr_ = new int[rhs.last_ + 1];
 		initSJCVector(rhs.size_);
-		//Copying the resource avoids double frees
+		// Copying the resource avoids double frees
 		const auto& srcBegin = rhs.ptr_.get();
 		std::copy(srcBegin, std::next(srcBegin, size_), ptr_.get());
 		last_ = rhs.last_;
 		rename("copy");
 	}
-	//Move constructor (copy constructor for rvalues)
-	//Move constructor transfers ownership of the resource from rhs to this
-	//Fast because rhs wont be missed, just steal rhs's guts.
+	// MOVE CONSTRUCTOR (copy constructor for rvalues)
+	// Move constructor transfers ownership of the resource from rhs to this
+	// Fast because rhs wont be missed, just steal rhs's guts.
 	SJCVector(SJCVector&& rhs) noexcept {
 		std::cout << "Move ctor. Stole guts of rvalue: "; rhs.printNameLn();
 		ptr_ = std::exchange(rhs.ptr_, nullptr);//ptr_ gets rhs.ptr_, rhs.ptr_ gets nullptr.
@@ -101,20 +137,18 @@ public:
 		last_ = std::exchange(rhs.last_, -1);
 	}
 #ifdef BY_VAL_OPERATOR
-	//By value assignment operator
-	//Less common is to replace both move assignment operators with a by-value assignment operator
-	//Because the copy assignment and move assignment operators are so similar
-	//Could get both by creating a pass by value assignment operator
-	//This approach transfers move semantics responsibility to the caller
-	SJCVector& operator=(SJCVector copy) {
+	// BY-VALUE ASSIGNMENT OPERATOR
+	// The copy & move assignment operators are similar. We get both by creating a by-value assignment operator.
+	// Less common. This approach transfers move semantics responsibility to the caller.
+	SJCVector& operator=(SJCVector copy) noexcept {
 		std::cout << "By-value assignment (=) operator\n";
 		copy.swap(*this);
 		return *this;
 	}
 #else
-	//Copy assignment operator
-	//Free the left hand resource and copy the right hand one
-	//Use the copy and swap idiom. This decouples any aliasing relationship between *this and rhs
+	// COPY ASSIGNMENT OPERATOR
+	// Free the left hand resource and copy the right hand one
+	// Use the copy and swap idiom. This decouples any aliasing relationship between *this and rhs
 	SJCVector& operator=(const SJCVector& rhs) {
 		std::cout << "Copy assignment operator. (Uses Copy constructor)\n";
 		SJCVector copy = rhs;	//make a copy of the rhs object using the copy constructor
@@ -123,8 +157,8 @@ public:
 		std::cout << "End of Copy assignment operator\n";
 		return *this;
 	}
-	//Move assignment operator
-	//Free the left-hand resource and transfer ownership of the rhs one
+	// MOVE ASSIGNMENT OPERATOR
+	// Free the left-hand resource and transfer ownership of the rhs one
 	SJCVector& operator=( SJCVector&& rhs) noexcept{
 		std::cout << "Move assignment operator. Uses Move constructor\n";
 		SJCVector copy(std::move(rhs));	//make a copy of the rhs object using the MOVE constructor
@@ -140,7 +174,8 @@ public:
 		swap(first_, rhs.first_);
 		swap(last_, rhs.last_);
 	}
-	//Two argument swap makes this type efficiently std::swappable
+	// TWO ARGUMENT SWAP
+	// Makes this class efficiently std::swappable
 	// This is a non member function using the hidden friend idiom.
 	// The friend function is in the body of our class ie in its namespace
 	// Marking as friend lets the compiler know it is not a member
@@ -149,8 +184,8 @@ public:
 		a.swap(b);
 	}
 	SJCVector operator+(const SJCVector& rhs) {
-		//Only works for types where '+" is defined
-		//This routine applies element by element addition
+		// Only works for types where '+" is defined
+		// This routine applies element by element addition
 		std::cout << "Addition operator overload for SJCVector\n";
 		if ((rhs.size_  == 0) 
 		|| (size_ == 0)
